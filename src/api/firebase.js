bb313ae45750f0c6d0352ec4c42d4c06f9570316
2zzzyoung <mzooyoungn@gmail.com>
2023-05-02 23:56:10 +0900

    firebase

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -4,9 +4,9 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: REACT_APP_FIREBASE_DB_URL,
-    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
