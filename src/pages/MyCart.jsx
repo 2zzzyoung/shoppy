5443b4ff0ddc69e9cbc49aa6178ad7bd7c9a2068
2zzzyoung <mzooyoungn@gmail.com>
2023-05-15 23:33:38 +0900

    cart page ui

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -6,6 +6,7 @@ import CartItem from "../components/CartItem";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FaEquals } from "react-icons/fa";
 import PriceCard from "../components/PriceCard";
+import Button from "../components/ui/Button";
 
 const SHIPPING = 3000;
 
@@ -20,22 +21,23 @@ export default function MyCart() {
         products && products.reduce((prev, current) => prev + parseInt(current.price) * current.quantity, 0);
 
     return (
-        <section>
-            <p>내 장바구니</p>
+        <section className="p-8 flex flex-col">
+            <p className="text-2cl text-center font-bold pb-4 border-b border-gray-300">내 장바구니</p>
             {!hasProducts && <p>장바구니에 상품이 없습니다.</p>}
             {hasProducts && (
                 <>
-                    <ul>
+                    <ul className="border-b border-gray-300 mb-8 p-4p px-8">
                         {products &&
                             products.map((product) => <CartItem key={product.id} product={product} uid={uid} />)}
                     </ul>
-                    <div>
+                    <div className="flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16">
                         <PriceCard text="상품 총액" price={totalPrice} />
-                        <BsFillPlusCircleFill />
+                        <BsFillPlusCircleFill className="shrink-0" />
                         <PriceCard text="배송비" price={SHIPPING} />
-                        <FaEquals />
+                        <FaEquals className="shrink-0" />
                         <PriceCard text="총 금액" price={totalPrice + SHIPPING} />
                     </div>
+                    <Button text="주문하기" />
                 </>
             )}
         </section>
